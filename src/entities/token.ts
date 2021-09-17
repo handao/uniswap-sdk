ee23d1be261f5450ee566fb2e41515f09e43b4f9
handao <44291031+handao@users.noreply.github.com>
2021-09-17 14:38:14 +0800

    Update token.ts

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -58,5 +58,5 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
 
 export const WETH = {
   [ChainId.MAINNET]: new Token(ChainId.MAINNET, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.TESTNET]: new Token(ChainId.TESTNET, '0x73DcdfeBe2b1Db8FAe6f0A5AA0f35C3BaDa6811A', 18, 'WETH', 'Wrapped Ether')
+  [ChainId.TESTNET]: new Token(ChainId.TESTNET, '0x2b1056710BBbD162Ce78478f6a965D2Cc47D0E04', 18, 'WETH', 'Wrapped Ether')
 }
